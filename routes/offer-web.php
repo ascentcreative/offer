c80a6713a60d47a3b666de91cb7ccbd258d3cf55
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2022-07-30 23:13:08 +0100

    autocomplete for sellables

--- routes/offer-web.php
+++ routes/offer-web.php
@@ -1,5 +1,7 @@
 <?php
 
+use AscentCreative\Offer\Facades\Discountables;
+
 
 Route::middleware('web')->group( function() {
 
@@ -15,9 +17,37 @@ Route::middleware('web')->group( function() {
 
     Route::prefix('/admin')->middleware(['auth', 'can:administer'])->group(function() {
 
+
+        Route::get('/offers/autocomplete/sellables', function() {
+
+            // dd();
+
+            $term = request()->term; 
+
+            $items = collect([]);
+
+            foreach(Discountables::getRegistry() as $cls) {
+                $items = $items->concat($cls::autocomplete($term)->get());
+            }
+
+            $items = collect($items)->transform( function($item) {
+                $item->label = $item->sellable_label;
+                return [
+                    'label' => $item->sellable_label,
+                    'type' => get_class($item),
+                    'id' => $item->id
+                ];
+            });
+
+            return $items;
+        
+        })->name('offers.autocomplete.sellables');
+    
+
         Route::get('/offers/{offer}/delete', [\AscentCreative\Offer\Controllers\Admin\OfferController::class, 'delete']);
         Route::resource('/offers', \AscentCreative\Offer\Controllers\Admin\OfferController::class, ['as'=>'admin']);
-    
+
+        
     });
 
 
