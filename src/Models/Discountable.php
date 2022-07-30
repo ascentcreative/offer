11a4544b33b972781a317ae2d8a9a20468d4bcc4
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2022-07-30 23:14:43 +0100

    Offer items / discountable

--- src/Models/Discountable.php
+++ src/Models/Discountable.php
@@ -7,12 +7,28 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 use Illuminate\Support\Facades\DB;
 
 
+
 class Discountable extends Pivot 
 {
     use HasFactory;
 
     public $table = 'offer_discountables';
 
-    public $fillable = [];
+    public $fillable = ['id', 'offer_id', 'discountable_type', 'discountable_id'];
+                                     
+    public function discountable() {
+        return $this->morphTo();
+    }
+
+    public function getLabelAttribute() {
+        $disc = $this->discountable;
+
+        if(method_exists($disc, 'resolveShippables')) {
+            return $disc->shippable_label;
+        } else {
+            return $disc->shippable_label;
+        }
+
+    }
 
 }
