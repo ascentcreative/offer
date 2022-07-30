284efaccace881a25c492745b59be3ec6d708fab
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2022-07-30 23:15:13 +0100

    Rule structure

--- src/Rules/AbstractRule.php
+++ src/Rules/AbstractRule.php
@@ -1,10 +1,18 @@
 <?php
 namespace AscentCreative\Offer\Rules;
 
+use AscentCreative\Offer\Models\Offer;
+
 abstract class AbstractRule {
 
     public function getDiscountables() {
     
     }
+
+    abstract static function apply(Offer $offer, $basket);
+
+    abstract static function remove(Offer $offer, $target);
+
+
 
 }
